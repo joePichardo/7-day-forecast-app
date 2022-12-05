a14b5ffa65827cc6200a7f4c8db7a4d5e760e578
joePichardo <joepichar@gmail.com>
2022-12-05 01:18:59 -0700

    get inputs and add event listener to form

--- public/weather.js
+++ public/weather.js
@@ -1,9 +1,12 @@
-async function getData() {
-	const city = "Phoenix";
-	const state = "AZ";
-	const country = "US";
+const form = document.getElementById('location-form');
+form.addEventListener("submit", async function(event) {
+	event.preventDefault();
+	const city = document.getElementById("input-city").value;
+	const state = document.getElementById("input-state").value;
+	const country = document.getElementById("input-country").value;
 	const data = await fetch(`/weather?city=${city}&state=${state}&country=${country}`)
-		.then(response => response.json())
-		.then(json => json);
+	.then(response => response.json())
+	.then(json => json);
+
 	console.log(data);
-}
+})
