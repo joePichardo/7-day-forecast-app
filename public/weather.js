fc261a995122eb80e178a61a82d272710ab47cc4
joePichardo <joepichar@gmail.com>
2022-12-05 15:37:06 -0700

    add loading state and location title on forecast, fix html on forecast days, and get initial forecast

--- public/weather.js
+++ public/weather.js
@@ -6,6 +6,12 @@ function googlePlacesInitialize() {
 			var place = autocomplete.getPlace();
 			console.log(place);
 			console.log(place.name);
+			document.getElementById('forecast').innerHTML = `<div class="loader"></div>`;
+			if (place.formatted_address) {
+				document.getElementById('forecast-location').innerHTML = `<h1>${place.formatted_address} - Forecast</h1>`;
+			} else {
+				document.getElementById('forecast-location').innerHTML = "";
+			}
 			if (place.geometry && place.geometry.location) {
 				console.log(place.geometry.location.lng());
 				console.log(place.geometry.location.lat());
@@ -34,15 +40,21 @@ async function getForecastData(lat, lng) {
 		const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 		const html = `
-		<div>
-			<div>${weekday[date.getDay()]}</div>
-			<div>${date.toLocaleDateString('en-US')}</div>
-			<div>${temperature}</div>
-			<div>
-				<img src="https://openweathermap.org/img/wn/${weather.icon}@4x.png" alt="Weather - ${weather.description}">
+		<div class="forecast__wrapper">
+			<div class="forecast__wrapper--padding">
+				<h2 class="forecast__weekday">${weekday[date.getDay()]}</h2>
+				<div class="forecast__date">${date.toLocaleDateString('en-US')}</div>
+				<div class="forecast__temperature">${temperature}&#176;</div>
+				<div class="forecast__weather">${weather.main}</div>
+				<div class="forecast__icon">
+					<img src="https://openweathermap.org/img/wn/${weather.icon}@4x.png" alt="Weather - ${weather.description}">
+				</div>
+				<div class="forecast__wind">
+					<div class="forecast__wind--title">Wind</div>
+					<div class="forecast__wind--speed">Speed: ${wind.speed} mph</div>
+					<div class="forecast__wind--direction">Direction: ${wind.deg} degrees</div>
+				</div>
 			</div>
-			<div>${weather.main}</div>
-			<div>Wind Speed ${wind.speed} and Direction ${wind.deg} degrees</div>
 		</div>
 		`
 
@@ -59,5 +71,7 @@ async function getForecastData(lat, lng) {
 		document.getElementById('forecast').innerHTML = htmlForecast;
 	}
 }
+
+getForecastData(33.5386523, -112.1859866);
 
 window.addEventListener('load', googlePlacesInitialize);
