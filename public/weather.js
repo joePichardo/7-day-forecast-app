9b68e8bc994dbddef31b6e725385b9e188cb9998
joePichardo <joepichar@gmail.com>
2022-12-05 19:09:32 -0700

    SEO and accessibility additions

--- public/weather.js
+++ public/weather.js
@@ -4,7 +4,7 @@ function googlePlacesInitialize() {
 	const autocomplete = new google.maps.places.Autocomplete(input);
 		google.maps.event.addListener(autocomplete, 'place_changed', function () {
 			var place = autocomplete.getPlace();
-			document.getElementById('forecast').innerHTML = `<div class="loader"></div>`;
+			document.getElementById('forecast').innerHTML = `<div class="loader" role="alert" aria-busy="true"></div>`;
 			if (place.formatted_address) {
 				document.getElementById('forecast-location').innerHTML = `<h1>${place.formatted_address} - Forecast</h1>`;
 			} else {
@@ -39,16 +39,16 @@ async function getForecastData(lat, lng) {
 		<div class="forecast__wrapper">
 			<div class="forecast__wrapper--padding">
 				<h2 class="forecast__weekday">${weekday[date.getDay()]}</h2>
-				<div class="forecast__date">${date.toLocaleDateString('en-US')}</div>
-				<div class="forecast__temperature">${temperature.day}&#176;</div>
-				<div class="forecast__weather">${weather.main}</div>
+				<p class="forecast__date">${date.toLocaleDateString('en-US')}</p>
+				<p class="forecast__temperature">${temperature.day}&#176;</p>
+				<p class="forecast__weather">${weather.main}</p>
 				<div class="forecast__icon">
 					<img src="https://openweathermap.org/img/wn/${weather.icon}@4x.png" alt="Weather - ${weather.description}">
 				</div>
 				<div class="forecast__wind">
-					<div class="forecast__wind--title">Wind</div>
-					<div class="forecast__wind--speed">Speed: ${wind_speed} mph</div>
-					<div class="forecast__wind--direction">Direction: ${wind_deg} degrees</div>
+					<p class="forecast__wind--title">Wind</p>
+					<p class="forecast__wind--speed">Speed: ${wind_speed} mph</p>
+					<p class="forecast__wind--direction">Direction: ${wind_deg} degrees</p>
 				</div>
 			</div>
 		</div>
